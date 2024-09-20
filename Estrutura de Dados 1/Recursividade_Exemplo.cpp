fba147d13842d01ab53a827ee85878150c2b1ddb
Randú <134299499+joserandu@users.noreply.github.com>
2024-09-20 11:13:10 -0300

    Update Recursividade_Exemplo.cpp

--- Estrutura de Dados 1/Recursividade_Exemplo.cpp
+++ Estrutura de Dados 1/Recursividade_Exemplo.cpp
@@ -8,9 +8,27 @@ int fat(int n) {
 }
 
 int main(){
+	
+	// Forma recursiva
 	int n = 5;
 	
 	printf("%d ", fat(n));
+	
+	/*As definições matemáticas são recursivas.*/
+	
+	// Forma iterativa (mais rápida e não usa stack
+	int result=1;
+
+	if (n != 0) {
+		for (int i; i<=n; i++) {
+	
+		result = result * i;
+		
+		}	
+ 	}
+		
+	printf("\n%d ", result);
+	
 }
 
 // Esse método é muito bom, mas se for com números muito grandes, como o 100, vai travar a máquina, por dar overflow
