c63a3560394718b9660a31d983b598514da83851
Randú <134299499+joserandu@users.noreply.github.com>
2024-11-25 11:26:37 -0300

    Create ClassePrincipal.java

---
+++ Engenharia de Software 1/Projeto de gerenciamento de estoque de mercado/ClassePrincipal.java
@@ -0,0 +1,10 @@
+package mercadin;
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Produto prod = Produto("Bolo", "Do Ronaldo", "30/11/2024", 200);
+		
+}
