40c3a3eb2b23618c2e68e3ad9966dbe9ddc6d719
Randú <134299499+joserandu@users.noreply.github.com>
2024-12-17 09:02:36 -0300

    Update main.py

--- Desenvolvimento Orientado a Objetos/avaliação2/empresa/main.py
+++ Desenvolvimento Orientado a Objetos/avaliação2/empresa/main.py
@@ -1,12 +1,21 @@
 # Corrigir chegando em casa
-with open("departamento_financeiro/README.txt", 'r') as dpf
-dp_financeiro = dpf.read()  
-print(dp_financeiro)
+try:
+  with open("departamento_financeiro/README.txt", 'r') as dpf
+    dp_financeiro = dpf.read()  
+    print(dp_financeiro)
+except FileNotFoundError:
+  print("Erro: Arquivo não encontrado.")
 
-with open("departamento_recursos_humanos/README.txt", 'r') as dprh
-  dp_recursos_humanos = dprh.read()
-  print(dp_recursos_humanos)
+try:
+  with open("departamento_recursos_humanos/README.txt", 'r') as dprh
+    dp_recursos_humanos = dprh.read()
+    print(dp_recursos_humanos)
+except FileNotFoundError:
+  print("Erro: Arquivo não encontrado.")
 
-with open("departamento_tecnologia/README.txt", 'r') as dpt
-  dp_tecnologia = dpt.read()
-  print(dp_tecnologia)
+try:
+  with open("departamento_tecnologia/README.txt", 'r') as dpt
+    dp_tecnologia = dpt.read()
+    print(dp_tecnologia)
+except FileNotFoundError:
+  print("Erro: Arquivo não encontrado.")
