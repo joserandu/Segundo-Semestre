1a7666d39ae0cc73ae991eefc6bc74c5a085db57
Randú <134299499+joserandu@users.noreply.github.com>
2025-01-06 13:57:50 -0300

    Update InsertionSort.c

--- Estrutura de Dados 1/Trabalho Final/InsertionSort.c
+++ Estrutura de Dados 1/Trabalho Final/InsertionSort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>     /* necess?rio p/ as fun??es rand() e srand() */
 #include <time.h>   	/* for clock_t, clock(), CLOCKS_PER_SEC      */
+#include <locale.h>
 
 #define TAMANHO 5000
 int vet[5000];
@@ -30,18 +31,24 @@ int le_arquivo(int TAM){	/* faz a abertura do arquivo e leitura de uma quantidad
 }
 
 int main(int argc, char *argv[]) {
-	int i, j;
-	int x;
+	setlocale(LC_ALL, "Portuguese");
+	
+	int i, j, x;
+	
+	double soma, media;
 	int ww;
 	int tam_ordenar = 5000;  /* quero ordenar apenas tam_ordenar */
+	double times[1000];
 		
-	le_arquivo(5000);
-		
-	double time_spent = 0.0; 	/* to store the execution time of code */
 	
-	clock_t begin = clock();		/* tempo inicial */
+	for (ww=0; ww<1000; ww++) {
+		
+		le_arquivo(5000);
+		
+		double time_spent = 0.0; 	/* to store the execution time of code */
+	
+		clock_t begin = clock();		/* tempo inicial */
 
-	for ( ww=0; ww<1000; ww++) {
 
 		for (i = 1; i < tam_ordenar; i++) { 
 		    x = vet[i];
@@ -50,22 +57,28 @@ int main(int argc, char *argv[]) {
    			}
     		vet[j] = x;
 		}
-		
-	}	
-		
-	clock_t end = clock();		/* tempo final */
+	
+		clock_t end = clock();		/* tempo final */
 
-	/* 	calculate elapsed time by finding difference (end - begin) and
-      		 dividing the difference by CLOCKS_PER_SEC to convert to seconds */
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+		/* 	calculate elapsed time by finding difference (end - begin) and
+	 	dividing the difference by CLOCKS_PER_SEC to convert to seconds */
+		time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+		
+		printf("%f\n", time_spent);
+	
+		times[ww] = time_spent;
+		soma = soma + times[ww];
+	}	
+
+	// Média dos tempos
+	media = soma / 1000;
+
+	printf("Média de tempo: %f", media);
 
 	printf("\n\nVetor ordenado ==> ");
 	for (i=0; i<tam_ordenar; i++){
 		printf(" %d ", vet[i]);
 	}
-	
-	printf("\nThe elapsed time is %f seconds", time_spent);
-	printf("\n\n");
 	
 	system ("PAUSE");
 	return 0;
