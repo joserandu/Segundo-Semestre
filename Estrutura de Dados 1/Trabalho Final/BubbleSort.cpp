0f8501144c7d10bd13a0e767494ef543a7cafc48
Randú <134299499+joserandu@users.noreply.github.com>
2025-01-15 10:58:29 -0300

    Update BubbleSort.cpp

--- Estrutura de Dados 1/Trabalho Final/BubbleSort.cpp
+++ Estrutura de Dados 1/Trabalho Final/BubbleSort.cpp
@@ -33,57 +33,93 @@ int le_arquivo(int TAM){	/* faz a abertura do arquivo e leitura de uma quantidad
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "Portuguese");
 	
-	// Variáveis do algorítmo de ordenação
 	int i, j, temp;	
 	
-	// Variáveis de configuração da atividade (Não apagar)
 	double soma, media;
-	int ww;
-	int tam_ordenar = 5000;  /* quero ordenar apenas tam_ordenar */
+	int ww, n;
 	double times[1000];
+	FILE *result_file, *result_file_n;
+
+    // Abrir um único arquivo para armazenar os tempos de execução
+    result_file = fopen("bubble_sort.txt", "w");
+    if (result_file == NULL) {
+        printf("Erro ao criar o arquivo de resultados.\n");
+        return -1;
+    }
+    
+    // Vetor com os tamanhos de entrada para cada execução
+    int tamanhos[] = {500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
+	int size_index;
+
+    // Para cada tamanho de entrada
+    for (size_index = 0; size_index < 10; size_index++) {
+        n = tamanhos[size_index];
+        
+        char nome_arquivo[99];
+        
+		sprintf(nome_arquivo, "bubble_sort_%d.txt", n);        
+        
+		// Abrir arquivo para armazenar os tempos de execução de n
+	    result_file_n = fopen(nome_arquivo, "w");
+	    if (result_file == NULL) {
+	        printf("Erro ao criar o arquivo de resultados.\n");
+	        return -1;
+	    }
+
+        // Escrever no arquivo o tamanho que está sendo executado
+        fprintf(result_file, "Tempo da ordenação com %d números:\n", n);
+        fprintf(result_file_n, "Tempo da ordenação com %d números:\n", n);
+
+        // Repetir o processo para 1000 execuções
+        soma = 0.0; // Resetar soma a cada novo tamanho
 		
 	
-	for (ww=0; ww<1000; ww++) {
+		for (ww=0; ww<1000; ww++) {
+			
+			le_arquivo(5000);  /* Lendo o arquivo novamente e armazenando os seus valores em vet */
+			
+			double time_spent = 0.0; 	/* to store the execution time of code */
 		
-		le_arquivo(5000);
-		
-		double time_spent = 0.0; 	/* to store the execution time of code */
-	
-		clock_t begin = clock();		/* tempo inicial */
-
-
-		for (i=0; i<tam_ordenar-1; i++){
-			for (j=0; j<tam_ordenar-i-1; j++){
-				if (vet[j] > vet[j+1]){
-					temp = vet[j];
-					vet[j] = vet[j+1];
-					vet[j+1] = temp;
+			clock_t begin = clock();		/* tempo inicial */
+	 
+			for (i=0; i<n-1; i++){
+				for (j=0; j<n-i-1; j++){
+					if (vet[j] > vet[j+1]){
+						temp = vet[j];
+						vet[j] = vet[j+1];
+						vet[j+1] = temp;
+					}
 				}
 			}
-		}
-
-		clock_t end = clock();		/* tempo final */
-
-		/* 	calculate elapsed time by finding difference (end - begin) and
-	 	dividing the difference by CLOCKS_PER_SEC to convert to seconds */
-		time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
 		
-		printf("%f\n", time_spent);
+			clock_t end = clock();		/* tempo final */
 	
-		times[ww] = time_spent;
-		soma = soma + times[ww];
-	}	
+			/* 	calculate elapsed time by finding difference (end - begin) and
+		 	dividing the difference by CLOCKS_PER_SEC to convert to seconds */
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+		    fprintf(result_file, "%f\n", time_spent);  // Armazenar o tempo da execução
 
-	// Média dos tempos
-	media = soma / 1000;
+		    fprintf(result_file_n, "%f\n", time_spent);  // Armazenar o tempo da execução no arquivo n
+		
+			times[ww] = time_spent;
+			soma = soma + times[ww];
+		}	
 
-	printf("Média de tempo: %f", media);
-
-	printf("\n\nVetor ordenado ==> ");
-	for (i=0; i<tam_ordenar; i++){
-		printf(" %d ", vet[i]);
+		// Média dos tempos
+		media = soma / 1000;
+	
+		printf("Média de tempo (%d números): %f\n", n, media);
+	    fprintf(result_file, "Tempo médio: %f\n\n", media);
+	    
+	    // Arquivo local
+		fprintf(result_file_n, "Tempo médio: %f\n\n", media);
+	    fclose(result_file_n);
+	
 	}
-	
+
+    fclose(result_file);  // Fechar o arquivo de resultados
+    printf("\n\nExecuções concluídas. Os resultados foram armazenados em 'bubble_sort.txt'.\n");
+
 	system ("PAUSE");
 	return 0;
 }
