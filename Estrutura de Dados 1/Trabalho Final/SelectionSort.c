76edeab4560e1fa81f92906af925f39442b9fd78
Randú <134299499+joserandu@users.noreply.github.com>
2025-01-15 11:00:06 -0300

    Update SelectionSort.c

--- Estrutura de Dados 1/Trabalho Final/SelectionSort.c
+++ Estrutura de Dados 1/Trabalho Final/SelectionSort.c
@@ -33,55 +33,91 @@ int le_arquivo(int TAM){	/* faz a abertura do arquivo e leitura de uma quantidad
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "Portuguese");
 	
-	int i, j, min, x;
+	int i, j, min, aux;
 	
 	double soma, media;
-	int ww;
+	int ww, n;
 	int tam_ordenar = 5000;  /* quero ordenar apenas tam_ordenar */
 	double times[1000];
-		
+	FILE *result_file, *result_file_n;
+	int size_index;
 	
-	for (ww=0; ww<1000; ww++) {
-		
-		le_arquivo(5000);
-		
-		double time_spent = 0.0; 	/* to store the execution time of code */
+	// Abrir um único arquivo para armazenar os tempos de execução
+    result_file = fopen("selection_sort.txt", "w");
+    if (result_file == NULL) {
+        printf("Erro ao criar o arquivo de resultados.\n");
+        return -1;
+    }
+    
+    // Vetor com os tamanhos de entrada para cada execução
+    int tamanhos[] = {500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
+    
+    // Para cada tamanho de entrada
+    for (size_index = 0; size_index < 10; size_index++) {
+        n = tamanhos[size_index];
+        
+        char nome_arquivo[99];
+        
+		sprintf(nome_arquivo, "selection_sort_%d.txt", n);        
+        
+		// Abrir arquivo para armazenar os tempos de execução de n
+	    result_file_n = fopen(nome_arquivo, "w");
+	    if (result_file == NULL) {
+	        printf("Erro ao criar o arquivo de resultados.\n");
+	        return -1;
+	    }
+
+        // Escrever no arquivo o tamanho que está sendo executado
+        fprintf(result_file, "Tempo da ordenação com %d números:\n", n);
+        fprintf(result_file_n, "Tempo da ordenação com %d números:\n", n);
+
+        // Repetir o processo para 1000 execuções
+        soma = 0.0; // Resetar soma a cada novo tamanho
 	
-		clock_t begin = clock();		/* tempo inicial */
-
-
-		for (i = 0; i < tam_ordenar; i++) {
-    		min = i;
-    		for (j = i + 1; j < tam_ordenar; j++) {
-        		if (vet[j] < vet[min]) {
-            	min = j;
-        		}
-   			}
-		    x = vet[min];
-		    vet[min] = vet[i];
-		    vet[i] = x;
-		}
-	
-		clock_t end = clock();		/* tempo final */
-
-		/* 	calculate elapsed time by finding difference (end - begin) and
-	 	dividing the difference by CLOCKS_PER_SEC to convert to seconds */
-		time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+		for (ww=0; ww<1000; ww++) {
+			
+			le_arquivo(5000);
+			
+			double time_spent = 0.0; 	/* to store the execution time of code */
 		
-		printf("%f\n", time_spent);
+			clock_t begin = clock();		/* tempo inicial */
 	
-		times[ww] = time_spent;
-		soma = soma + times[ww];
-	}	
+	
+			for (i = 0; i < n; i++) {
+	    		min = i;
+	    		for (j = i + 1; j < n; j++) {
+	        		if (vet[j] < vet[min]) {
+	            		min = j;
+	        		}
+	   			}
+			    aux = vet[min];
+			    vet[min] = vet[i];
+			    vet[i] = aux;
+			}
+		
+			clock_t end = clock();		/* tempo final */
+	
+			/* 	calculate elapsed time by finding difference (end - begin) and
+		 	dividing the difference by CLOCKS_PER_SEC to convert to seconds */
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+		    
+			fprintf(result_file, "%f\n", time_spent);  // APAGAR DEPOISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
 
-	// Média dos tempos
-	media = soma / 1000;
-
-	printf("Média de tempo: %f", media);
-
-	printf("\n\nVetor ordenado ==> ");
-	for (i=0; i<tam_ordenar; i++){
-		printf(" %d ", vet[i]);
+		    fprintf(result_file_n, "%f\n", time_spent);  // Armazenar o tempo da execução no arquivo n
+		
+			times[ww] = time_spent;
+			soma = soma + times[ww];
+		}	
+	
+		// Média dos tempos
+		media = soma / 1000;
+		
+		printf("Média de tempo (%d números): %f\n", n, media);
+	    fprintf(result_file, "Tempo médio: %f\n\n", media);
+	    
+	    // Arquivo local
+		fprintf(result_file_n, "Tempo médio: %f\n\n", media);
+	    fclose(result_file_n);
 	}
 	
 	system ("PAUSE");
